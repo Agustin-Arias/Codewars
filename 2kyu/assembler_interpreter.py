4da80d9ac5e96c2cc9f1d71f1a011c3805366135
gustin33 <gustin444@gmail.com>
2020-05-29 17:05:56 -0300

    Almost completed, message and \t issues need fixing

--- 2kyu/assembler_interpreter.py
+++ 2kyu/assembler_interpreter.py
@@ -1,109 +1,3 @@
-# import re
-
-# def make_output(msg_parameters, registers):
-#     print(msg_parameters)
-#     output = ''
-#     count = 0
-#     for parameter in msg_parameters:
-#         if count == 0:
-#             if parameter == "'":
-#                 count += 1
-#             elif parameter == parameter.strip("'"):
-#                 # parameter was a register
-#                 print(parameter)
-#                 output += str(registers[parameter])
-#             else:
-#                 # parameter was a string:
-#                 output += parameter.strip("'")
-#         elif count == 1 and parameter == "'":
-#             output += ','
-#             count = 0
-#     return output
-
-
-# def assembler_interpreter(program):
-#     prog = list(filter(lambda x : x!= '', program.splitlines()))
-#     labels = set_labels(prog)  # label: line_pointer
-#     registers = {}
-#     line_pointer = 0 # vertically
-
-#     while line_pointer < len(prog):
-#         skip = False
-#         line = remove_empty_strings(prog[line_pointer].split(' '))
-#         print(' '.join(line), line_pointer)
-#         # print(f'registers: {registers}')
-#         word = line[0]
-
-#         if word == ';':
-#             line_pointer += 1
-#             skip = True
-
-#         elif word == 'cmp':
-#             x, y = line[1].strip(','), line[2]
-#             greater, equal = get_value(x, registers) > get_value(y, registers), get_value(x, registers) == get_value(y, registers)
-
-#         elif word == 'jne':
-#             lbl = line[1]
-#             if not equal:
-#                 line_pointer = labels[lbl]
-#                 skip = True
-
-#         elif word == 'je':
-#             lbl = line[1]
-#             if equal:
-#                 line_pointer = labels[lbl]
-#                 skip = True
-
-#         elif word == 'jge':
-#             lbl = line[1]
-#             if greater or equal:
-#                 line_pointer = labels[lbl]
-#                 skip = True
-
-#         elif word == 'jg':
-#             lbl = line[1]
-#             if greater:
-#                 line_pointer = labels[lbl]
-#                 skip = True
-
-#         elif word == 'jle':
-#             lbl = line[1]
-#             if not greater or equal:
-#                 line_pointer = labels[lbl]
-#                 skip = True
-
-#         elif word == 'jl':
-#             lbl = line[1]
-#             if not greater:
-#                 line_pointer = labels[lbl]
-#                 skip = True
-
-#         elif word == 'call':
-#             line_pointer_call = line_pointer  # we set the references
-#             lbl = line[1]
-#             line_pointer = labels[lbl]  # we jump to the label
-#             # print(f"line_pointer: {line_pointer}")
-#             skip = True
-
-#         elif word == 'msg':
-#             string_line = prog[line_pointer]
-#             msg_parameters = re.search(r"msg (.*) ;?", string_line).groups()[0].split(',')
-#             msg_parameters = remove_empty_strings([x.strip(" ") for x in msg_parameters])
-#             output = make_output(msg_parameters, registers)
-#             line_pointer += 1
-#             skip = True
-
-#         elif word == 'end':
-#             return output
-
-#         import time, sys
-#         time.sleep(1)
-#         sys.stdout.flush()
-
-#         if not skip:
-
-#     return -1
-
 def set_up_commands():
     global mov, inc, dec, add, sub, mul, div, jmp, cmp, jne, je, jge, jg, jle, jl, call, ret, msg, end, comment
     def mov(args):
@@ -128,9 +22,8 @@ def set_up_commands():
         x, y = args[0].strip(', '), args[1]
         registers[x] //= get_value(y, registers)
     def jmp(args):
-        global line_number, line_number_reference
+        global line_number
         lbl = args[0]
-        line_number_reference = line_number
         line_number = labels[lbl]
     def cmp(args):
         '''
@@ -140,29 +33,64 @@ def set_up_commands():
         '''
         global compare
         x, y = args[0].strip(', '), args[1]
+        x, y = get_value(x, registers), get_value(y, registers)
         compare = (x > y) - (x < y)
-    def jne(args): pass
-    def je(args): pass
-    def jge(args): pass
-    def jg(args): pass
-    def jle(args): pass
-    def jl(args): pass
-    def call(args): pass
+    def jne(args):
+        global line_number
+        if compare != 0:
+            lbl = args[0]
+            line_number = labels[lbl]
+    def je(args):
+        global line_number
+        if compare == 0:
+            lbl = args[0]
+            line_number = labels[lbl]
+    def jge(args):
+        global line_number
+        if compare >= 0:
+            lbl = args[0]
+            line_number = labels[lbl]
+    def jg(args):
+        global line_number
+        if compare == 1:
+            lbl = args[0]
+            line_number = labels[lbl]
+    def jle(args):
+        global line_number
+        if compare <= 0:
+            lbl = args[0]
+            line_number = labels[lbl]
+    def jl(args):
+        global line_number
+        if compare == -1:
+            lbl = args[0]
+            line_number = labels[lbl]
+    def call(args):
+        global line_number, line_number_reference, in_a_function
+        lbl = args[0]
+        if not in_a_function: line_number_reference, in_a_function = line_number, True
+        line_number = labels[lbl]
     def ret(args):
-        global line_number
+        global line_number, line_number_reference, in_a_function
         line_number = line_number_reference
-    def msg(args): pass
+        in_a_function = False
+    def msg(args):
+        global output
+        output = make_msg(message)
     def end(args):
-        global line_number
+        global line_number, program_ended_successfully
+        program_ended_successfully = True
         line_number = total_lines
     def comment(args): pass
 def set_up_environment(program):
-    global processed_program, total_lines, registers, labels, commands, line_number
+    global processed_program, total_lines, registers, labels, commands, line_number, in_a_function
     line_number = 0
-    processed_program = remove_empty_spaces(remove_empty_lines(program))
+    in_a_function = False
+    processed_program = remove_empty_spaces(remove_empty_strings(program.split('\n')))
     total_lines = len(processed_program)
     registers = {}
     labels = set_labels(processed_program)
+    find_message(program)
     commands = {
         'mov' : mov,
         'inc' : inc,
@@ -185,6 +113,15 @@ def set_up_environment(program):
         'end' : end,
         ';' : comment
     }
+def find_message(program):
+    global message
+    for line in program.split('\n'):
+        if 'msg' in line:
+            index = line.index('msg')
+            message = line[3:].strip(' ')
+def process_program(program):
+    global processed_program
+    processed_program = remove_empty_spaces(remove_empty_strings(program.split('\n')))
 def set_labels(processed_program):
     labels = {}
     for index, line in enumerate(processed_program):
@@ -195,35 +132,63 @@ def get_value(x, registers):
     if x in registers.keys():
         return registers[x]
     return int(x)
-def remove_empty_lines(program): return list(filter(lambda x: x.strip('') != '', program.split('\n')))
+def make_msg(string):
+    import re
+    pattern = r"(', '|.|'.*?')"
+    remove_comment = string[:string.index(';')].strip(' ') if ';' in string else string.strip(' ')
+    message = re.findall(f"{pattern},", remove_comment) + re.findall(f", {pattern}$", remove_comment)
+    print(message)
+    output = ''
+    for word in message:
+        if word == ';':
+            break
+        elif word[0] == "'":  # if the word is a literal message, we join it with the ouput
+            output += word.strip("'")
+        else:
+            output += str(get_value(word, registers))
+
+    return output
+def remove_empty_strings(string_list): return list(filter(lambda x: x.strip('') != '', string_list))
 def remove_empty_spaces(processed_program): return [line.strip(' ') for line in processed_program]
-def assembler_interpreter(program):
-    global line_number
+def interpret_code(processed_program):
+    global line_number, program_ended_successfully
+    program_ended_successfully = False
     while line_number < total_lines:
-        line = processed_program[line_number].split(' ')
+        line = remove_empty_strings(processed_program[line_number].split(' '))
         command, args = line[0], line[1:]
+        # print(line)
         do_command  = commands[command]
         do_command(args)
+        # print(registers)
         line_number += 1
+    if program_ended_successfully:
+        return output
+    return -1
+def assembler_interpreter(program):
+    set_up_commands()
+    set_up_environment(program)
+    process_program(program)
+    print(message)
+    set_labels(processed_program)
+    return interpret_code(processed_program)
+
+
 
 program = '''
-mov a, 2
-inc a
-add a, 4
-jmp function
-cmp a, b
+mov c, 11   ; instruction mov c, 11
+mov n, 6   ; instruction mov n, 6
+call func
+msg 'Random result: ', i
 end
 
-function:
-    mov b, 5
-    sub a, b
+func:
+    cmp c, n
+    jl exit
+    mov i, c
+    mul i, n
     ret
+; Do nothing
+exit:
+    msg 'Do nothing'
 '''
-set_up_commands()
-set_up_environment(program)
-processed_program = remove_empty_spaces(remove_empty_lines(program))
-set_labels(processed_program)
-assembler_interpreter(program)
-
-
-print(registers)
+print(assembler_interpreter(program))
