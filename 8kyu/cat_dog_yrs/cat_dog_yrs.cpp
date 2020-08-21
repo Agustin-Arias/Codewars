0f42994914d89b78a3780001ed8de6791d21f2dc
gustin33 <gustin444@gmail.com>
2020-08-20 22:35:15 -0300

    Deleted files

--- 8kyu/cat_dog_yrs/cat_dog_yrs.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-
-std::vector<int> humanYearsCatYearsDogYears(int hy) {
-    int catYears = (hy < 2 ? 15: 15 + 9);
-    int dogYears = catYears;
-    catYears += (hy > 2 ? (hy-2)*4 : 0);
-    dogYears += (hy > 2 ? (hy-2)*5 : 0);
-    return {hy, catYears, dogYears};
-}
